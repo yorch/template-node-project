53d9bdbc14d8be38364c09d382632c8a5da88f5c
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-03-14 11:30:21 -0400

    Cleanup

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
-const { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION } = process.env;
+const { API_KEY } = process.env;
 
 export const config = {
-    aws: {
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        apiVersion: '2012-08-10',
-        secretAccessKey: AWS_SECRET_ACCESS_KEY,
-        region: AWS_REGION,
-    },
+    apiKey: API_KEY,
 };
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import createLogger from 'logging';
-import { createTables } from './dynamo-db';
-import { getNews } from './get-news';
-import { getRecipients } from './get-recipients';
-import { processNewsItems } from './process-news-items';
-import { runCronTask } from './run-cron-task';
-import { sendSms } from './send-sms';
+import { apiKey } from './config';
 
-const logger = createLogger('Main');
-
-export const run = async () => {
-    logger.info('Started');
-
-    await createTables();
-
-    runCronTask(async () => {
-        const items = await getNews();
-        const { content } = await processNewsItems(items);
-        await sendSms({
-            body: content,
-            recipients: await getRecipients(),
-        });
-    });
-};
-
-run();
+(async () => {
+    console.log(`API Key is ${apiKey}`);
+})();
